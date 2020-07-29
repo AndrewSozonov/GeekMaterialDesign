e70c75bdee28f63d83cb300f2d8c672dd3d46a9c
Andrey Sozonov <Asozonov@yandex.ru>
2020-07-29 22:30:17 +0300

    Material Design task3
    ButtonActivity with examples of different buttons.
    ToolbarActivity with toolbar example.

---
+++ app/src/main/java/ru/andreysozonov/geekmaterialdesign/ButtonsActivity.java
@@ -0,0 +1,14 @@
+package ru.andreysozonov.geekmaterialdesign;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ButtonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+    }
+}
